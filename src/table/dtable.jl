7dc138b2ed91f593dfad2ca598c0470e4806dc73
krynju <gulinski.krystian@gmail.com>
2021-07-18 12:53:52 +0200

    fix naming/cleanup

--- src/table/dtable.jl
+++ src/table/dtable.jl
@@ -4,8 +4,7 @@ import Tables
 import Base: fetch, filter, map, setproperty!, eachrow
 
 mutable struct DTable
-    v::Vector{Dagger.EagerThunk}
-    thunksize::Int
+    chunks::Vector{Dagger.EagerThunk}
 end
 
 function DTable(table::DataFrames.DataFrame; chunksize=10)
@@ -15,17 +14,17 @@ function DTable(table::DataFrames.DataFrame; chunksize=10)
     r = Dagger.@spawn Tables.rows(table)
     n = DataFrames.nrow(table)
     partition_rows = (x, l, r) -> getindex(x, l:r)
-    df_create = (rows,i) -> DataFrames.DataFrame( partition_rows(rows, i, (i + thunksize - 1) % (n + 1)))
-    return DTable([Dagger.@spawn df_create(r,i) for i in 1:thunksize:n], thunksize)
+    df_create = (rows,i) -> DataFrames.DataFrame( partition_rows(rows, i, (i + chunksize - 1) % (n + 1)))
+    return DTable([Dagger.@spawn df_create(r,i) for i in 1:chunksize:n])
 end
 
 function filter(f, d::DTable)
     _f = x -> Dagger.@spawn filter(f, x)
-    DTable(map(_f, d.v), d.thunksize)
+    DTable(map(_f, d.chunks))
 end
 
 function fetch(d::DTable)
-    _fetch_thunk_vector(d.v)
+    _fetch_thunk_vector(d.chunks)
 end
 
 function _fetch_thunk_vector(x)
@@ -34,13 +33,13 @@ end
 
 function fetchcolumn(d::DTable, s::Symbol)
     _f = (x) -> Dagger.@spawn getindex(x, :, s)
-    _fetch_thunk_vector(map(_f, d.v))
+    _fetch_thunk_vector(map(_f, d.chunks))
 end
 
 function map(f, d::DTable)
     thunk_f = x -> Dagger.@spawn eachrow(x) # eachrow baked in here for now
     row_f = x -> Dagger.@spawn map(f, thunk_f(x))
-    DTable(map(row_f, d.v), d.thunksize)
+    DTable(map(row_f, d.chunks))
 end
 
-export DTable, getrow, apply, fetchcolumn
+export DTable
